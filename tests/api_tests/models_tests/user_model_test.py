ca38ad9a0bbc8e423ad710625211a256bdad9985
Gavin van der Merwe <g.vandermerwe@tarabutgateway.com>
2023-02-05 19:19:05 +0000

    Cannot get this to work, too many depdencies are bound together with the ORM, this will be a pain to maintain in the long run

--- tests/api_tests/models_tests/user_model_test.py
+++ tests/api_tests/models_tests/user_model_test.py
@@ -1,17 +1,27 @@
 import pytest
 from tortoise import Tortoise
+from tortoise.contrib.test import finalizer, initializer
 from api.models.user_model import User
 
+@pytest.mark.asyncio
 @pytest.fixture(scope='class')
-def tortoise_init():
-    Tortoise.init(db_url="sqlite://:memory:", modules={"models": [User]})
-    Tortoise.generate_schemas()
+async def tortoise_init():
+    await Tortoise.init(db_url='sqlite://:memory:', modules={'models': [User]})
+    await Tortoise.generate_schemas()
+    initializer(['api.models'])
 
+@pytest.mark.asyncio
 @pytest.mark.usefixtures('tortoise_init')
 class TestUserModel:
 
-    def test_insert(self):
-        print('test user')
+    test_user = User(username = 'Katya', password = 'password123')
 
-    def test_query(self):
-        print('test query')
+    @pytest.mark.asyncio
+    async def test_insert(self):
+        await self.test_user.save()
+    
+    @pytest.mark.asyncio
+    async def test_query(self):
+        queried_user = await User.filter(id=self.test_user.id)
+        assert queried_user.username == 'Katya'
+        assert queried_user.password == 'password123'
