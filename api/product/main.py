5ad582ad543e31a4e1d74f10360549885f418799
Indu Aggarwal <induaggarwal@Indus-MacBook-Pro.local>
2023-02-26 13:27:46 +0000

    added API router to separate the product and sellers routes in different files

--- api/product/main.py
+++ api/product/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends, Form, status, Response, HTTPException
 from .import schemas
 from .import models
-from .database import engine, SessionLocal
+from .database import engine, SessionLocal, get_db
 from typing import List
 from passlib.context import CryptContext
+from .routers import product
 
 
 
@@ -29,59 +30,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
 
 models.Base.metadata.create_all(engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close
+app.include_router(product.router)
 
 
-@app.delete('/product/{id}', tags=['Products'])
-def delete(id, db: SessionLocal = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
-    db.commit()
-    return{"Product is deleted"}
 
-
-@app.get('/products', response_model=List[schemas.DisplayProduct], tags=['Products'])
-def getproducts(db: SessionLocal = Depends(get_db)):
-    products = db.query(models.Product).all()
-    return products
-
-@app.get('/product/{id}', response_model=schemas.DisplayProduct, tags=['Products'])
-def product(id, db: SessionLocal = Depends(get_db)):
-     product = db.query(models.Product).filter(models.Product.id == id).first()
-     if not product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail='Product is not found')
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'Product not found'}
-     return product
-
-@app.put('/product/{id}', tags=['Products'])
-def update(id, request: schemas.Product, db: SessionLocal = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id)
-    if not product.first():
-        pass
-    product.update(request.dict())
-    db.commit()
-    # db.refresh(product)
-    return {'Product successfully updated'}
-    # return {product}
-
-
-# @app.post('/product', status_code=status.HTTP_201_CREATED)
-@app.post('/product',response_model=schemas.DisplayProduct, 
-            status_code=status.HTTP_201_CREATED,tags=['Products'])
-def add(request: schemas.Product, db:SessionLocal = Depends(get_db)):
-    new_product = models.Product(
-        name= request.name, description= request.description, price= request.price, seller_id= 1
-    )
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-    return new_product
 
 @app.post('/seller', response_model=schemas.DisplaySeller, tags=['Sellers'])
 def create_seller(request: schemas.Seller, db:SessionLocal = Depends(get_db)):
